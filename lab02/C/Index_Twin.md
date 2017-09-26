be867b229d4507ec3721cb82f0b571b93d9da843
edwardfang <fangyd1997@outlook.com>
2017-09-26 04:59:28 -0700

    C wrong version

--- lab02/C/Index_Twin.md
+++ lab02/C/Index_Twin.md
@@ -4,9 +4,9 @@ Time Limit: 1 Sec  Memory Limit: 128 MB
 
 #### Description
 
-Given an sorted integer array A and a specific target m. Cuperman defines (i,j) is index twin if A[i] + A[j] = m. Could you help Cuperman to find how many index twins are in the given array A with a specific target m.
+Given an sorted `ascending` integer array A and a specific target m. Cuperman defines (i,j) is index twin if A[i] + A[j] = m. Could you help Cuperman to find how many index twins are in the given array A with a specific target m.
 Input
-
+so
 The first line will be an integer T(1<T<=10), which is the number of test cases.
 
 For each case contain two lines.The first line contains two numbers n(1<=n<=5000) and m(1<=m<=108), n is the size of array A. m is the specified target.
--- lab02/C/solution.cpp
+++ lab02/C/solution.cpp
@@ -2,20 +2,62 @@
 
 using namespace std;
 
-int main(){
-    int T,i =0, n;
+int main()
+{
+    int T, i = 0, n, j;
     long m;
-    cin >> total;
-    for(i=0;i<T;i++){
+    cin >> T;
+    for (i = 0; i < T; i++)
+    {
+        int count = 0;
         cin >> n >> m;
-        long numbers[n];
-        long needs[n]
-        memset(numbers,0,sizeof(numbers));
-        for(j=0;j<n;j++){
+        long numbers[n], needs[n];
+        memset(numbers, 0, sizeof(numbers));
+        for (j = 0; j < n; j++)
+        {
             cin >> numbers[j];
+            if (numbers[j] > m)
+            {
+                break;
+            }
+            else
+            {
+                needs[j] = m - numbers[j];
+            }
+        }
+        int l, r, mid, k = 0, upper_bound = j;
+        for (k = 0; k <= j; k++)
+        {
+            l = 0;
+            r = upper_bound;
+            // binary search needs[j] and update lower bound
+            while (l < r - 1)
+            {
+                mid = (l + r) / 2;
+                cout << l << "-" << r << "-" << mid << endl;
+                if (numbers[mid] < needs[k])
+                {
+                    l = mid;
+                }
+                else if (numbers[mid] > needs[k])
+                {
+                    r = mid;
+                    upper_bound = r;
+                }
+                else
+                {
+                    cout<<"ok" << numbers[mid] << endl;
+                    (k - mid)*(m-needs[k]-numbers[mid])
+                    count++;
+                    break;
+                }
+            }
+            if ((l < r - 1) && (numbers[l] == needs[k] || numbers[r] == needs[k]))
+            {
+                count++;
+            }
         }
 
-        // quick sort needs[] and 
-        // search
+        cout << count <<endl;
     }
 }
