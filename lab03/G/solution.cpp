351dcd539fd106552a5337474416de7d33ea6f95
edwardfang <fangyd1997@outlook.com>
2017-10-08 01:52:53 -0700

    bug dead loop

--- lab03/G/solution.cpp
+++ lab03/G/solution.cpp
@@ -3,6 +3,42 @@
 
 using namespace std;
 
+int move_median(set<int> sortedlst[], set<int>::iterator &cur_it, int cur_idx, int left_right)
+{
+    switch (left_right)
+    {
+    case 0: //left
+        if (sortedlst[cur_idx].begin() != cur_it)
+        {
+            cur_it--;
+            return cur_idx;
+        }
+        else
+        {
+            set<int>::iterator it = sortedlst[cur_idx - 1].end();
+            cur_it = --it;
+            return --cur_idx;
+        }
+        break;
+    case 1: //right
+        //cout << "s3" << endl;
+        set<int>::iterator it = cur_it;
+        if (++it == sortedlst[cur_idx].end())
+        {
+            //cout << "s0" << endl;
+            cur_it = sortedlst[++cur_idx].begin();
+            return cur_idx;
+        }
+        else
+        {
+            //cout << "s1" << endl;
+            cur_it++;
+            return cur_idx;
+        }
+        break;
+    }
+}
+
 int main()
 {
     int T, i, j, n;
@@ -10,29 +46,39 @@ int main()
     for (i = 0; i < T; i++)
     {
         cin >> n;
-        int tmp, bucket_idx, median_idx, k, prev_sum;
+        int tmp, bucket_idx, k, prev;
+        set<int>::iterator median_it;
         set<int> sortedlst[100]; //10 buckets
         for (j = 0; j < n; j++)
         {
             cin >> tmp;
             bucket_idx = tmp / 100;
             sortedlst[bucket_idx].insert(tmp);
+            if (j == 0)
+            {
+                k = bucket_idx;
+                median_it = sortedlst[k].begin();
+            }
+            else if (j % 2 == 1)
+            {
+                prev = tmp;
+            }
+            else
+            {
+                //cout << "s2" << endl;
+                //cout << "input " << tmp << "k " << k << "median " << *median_it << endl;
+                if (prev < *median_it && tmp < *median_it)
+                {
+                    k = move_median(sortedlst, median_it, k, 0);
+                }
+                else if (prev > *median_it && tmp > *median_it)
+                {
+                    k = move_median(sortedlst, median_it, k, 1);
+                }
+            }
             if (j % 2 == 0)
             {
-                median_idx = j / 2;
-                for (k = 0; k < 100; k++)
-                {
-
-                    if (prev_sum + sortedlst[k].size() - 1 >= median_idx)
-                    {
-                        break;
-                    }
-                    prev_sum += sortedlst[k].size();
-                }
-                //cout << "sum " << prev_sum << "k " << k << endl;
-                set<int>::iterator it = sortedlst[k].begin();
-                std::advance(it, median_idx - prev_sum);
-                cout << *it;
+                cout << *median_it;
                 if (j != n - 1 && j != n - 2)
                     cout << " ";
             }
