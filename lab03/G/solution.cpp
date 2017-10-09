3d7802921adcdf74e3d948bd7be3534978006a1c
edwardfang <fangyd1997@outlook.com>
2017-10-09 02:38:41 -0700

    G finish; no more

--- lab03/G/solution.cpp
+++ lab03/G/solution.cpp
@@ -37,19 +37,20 @@ int main()
         advance(node_median, n / 2);
         // tranvrse reversely
         list<int>::iterator removed[2];
-        int move = 0, result[n], k = 0;
+        int move = 0, result[n], k = 0, odd =1;
         result[k++] = *node_median;
-        removed[0] = node_seq[j];
+        removed[0] = node_seq[n-1];
         for (j = n - 2; j > -1; j--)
         {
             //cout << "size" << linkedlist.size()<< endl;
-            if (j % 2 == 1)
+            if (odd)
             {
                 removed[1] = node_seq[j];
+                odd = 0;
             }
             else
             {
-
+                odd = 1;
                 // update median node
                 move = 0;
                 if (*removed[0] < *node_median && *removed[1] < *node_median)
