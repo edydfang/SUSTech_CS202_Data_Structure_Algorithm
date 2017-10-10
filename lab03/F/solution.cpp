5dd264c0c7953c531fd85a9dbcb1bd21d3b90c33
edwardfang <fangyd1997@outlook.com>
2017-10-09 19:18:26 -0700

    finish all

--- lab03/F/solution.cpp
+++ lab03/F/solution.cpp
@@ -1,219 +1,110 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
 #include <cstring>
-
+#include <vector>
+#include <utility>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <map>
+ 
+const int MAXC = 1000000 + 10; 
+const int MAXM = 1000 + 10; 
+const int MAXN = 1000000 + 10;
+const int N=2000, L=2000;
 using namespace std;
-
-const int N = 2000000 + 10, block_sz = 4000 + 100, block_num = 4000 + 10;
-
-queue<int> que;
-int head;
-char str[N];
-struct block
-{
-    int sz, next;
-    bool rev;
-    char s[block_sz];
-    void init()
-    {
-        sz = 0, next = -1, rev = 0;
-    }
-} g[block_num];
-int new_block()
-{
-    int t = que.front();que.pop();
-    g[t].init();
-    return t;
-}
-void del_block(int t)
-{
-    que.push(t);
-}
-void block_update(int idx)
-{
-    if(g[idx].rev)
-    {
-        reverse(g[idx].s, g[idx].s + g[idx].sz);
-        g[idx].rev = 0;
-    }
-}
-void block_split(int idx, int k)
-{
-    if(idx == -1 || k == g[idx].sz) return;
-    block_update(idx);
-    int tot = new_block();
-    memcpy(g[tot].s, g[idx].s + k, sizeof(char) * (g[idx].sz - k));
-    g[tot].sz = g[idx].sz - k, g[idx].sz = k;
-    g[tot].next = g[idx].next, g[idx].next = tot;
-}
-void block_merge(int idx)
-{
-    for(int i = idx; i != -1; i = g[i].next)
-    {
-        for(int j = g[i].next; j != -1; j = g[j].next)
-        {
-            if(g[i].sz + g[j].sz <= block_sz)
-            {
-                block_update(i), block_update(j);
-                memcpy(g[i].s + g[i].sz, g[j].s, sizeof(char) * g[j].sz);
-                g[i].sz += g[j].sz, g[i].next = g[j].next;
-                del_block(j);
-            }
-            else break;
+struct Block_List {
+    struct Node {
+        char str[L];
+       
+        int next, size;
+        void init(){
+             memset(str, 0, sizeof(str));
+             next = -1;
+             size = 0;
         }
+    }list[N];
+    int head, tot;
+ 
+    void init(char str[]){
+         head = tot = 0;
+         list[tot++].init();
+         for (int i = 0, cur = head; str[i]; cur = list[cur].next){
+             for (int j = 0; j < L && str[i]; j++, i++){
+                 list[cur].str[j] = str[i];
+                 list[cur].size++;
+             }
+              
+             if (str[i]){
+                list[tot].init();
+                list[cur].next = tot++; 
+             }
+         } 
+         for (int cur = head; cur != -1; cur = list[cur].next)
+         if (list[cur].size == L) split(cur);
     }
-}
-void block_init()
-{
-    while(! que.empty()) que.pop();
-    for(int i = 0; i < block_num; i++) que.push(i);
-    head = new_block();
-}
-void block_locate(int &idx, int &k)
-{
-    while(idx != -1 && k > g[idx].sz) k -= g[idx].sz, idx = g[idx].next;
-}
-void block_insert(int k, int n, char *str)
-{
-    int idx = head;
-    block_locate(idx, k);
-    block_split(idx, k);
-    int i = 0;
-    while(i < n)
-    {
-        int sz = min(block_sz, n - i);
-        int tot = new_block();
-        memcpy(g[tot].s, str + i, sizeof(char) * sz);
-        g[tot].sz = sz;
-        g[tot].next = g[idx].next, g[idx].next = tot, idx = g[idx].next;
-        i += sz;
+ 
+    void split(int x){
+         list[tot].init();
+         for (int i = L / 2; i < L; i++){
+             list[tot].str[i - L/2] = list[x].str[i];
+             list[tot].size++;
+             list[x].size--;
+             list[x].str[i] = 0;
+         }
+         list[tot].next = list[x].next;
+         list[x].next = tot++;
     }
-    block_merge(head);
-}
-void block_delete(int l, int r)
-{
-    int idx = head;
-    block_locate(idx, l);
-    block_split(idx, l);
-    int st = idx, st_next = g[idx].next;
-
-    idx = head;
-    block_locate(idx, r);
-    block_split(idx, r);
-    int en = idx, en_next = g[idx].next;
-
-    for(int i = st_next; i != en_next; i = g[i].next) del_block(i);
-    g[st].next = en_next;
-    block_merge(head);
-}
-void block_rev(int l, int r)
-{
-    int idx = head;
-    block_locate(idx, l);
-    block_split(idx, l);
-    int st = idx, st_next = g[idx].next;
-
-    idx = head;
-    block_locate(idx, r);
-    block_split(idx, r);
-    int en = idx, en_next = g[idx].next;
-
-    int tmp[block_num], cnt = 0;
-    for(int i = st_next; i != en_next; i = g[i].next)
-    {
-        g[i].rev ^= 1;
-        tmp[++cnt] = i;
+    void insert(int pos, char val){
+         int cur = head;
+        
+         while (pos - list[cur].size > 0 && list[cur].next != -1){
+               pos -= list[cur].size;
+               cur = list[cur].next; 
+         }
+         if (pos >= list[cur].size) list[cur].str[list[cur].size] = val;
+         else {
+             
+              for (int i = list[cur].size; i > pos; i--) list[cur].str[i] = list[cur].str[i - 1] ;
+              list[cur].str[pos] = val;
+         }
+         list[cur].size++;
+         if (list[cur].size == L) split(cur);
     }
-    tmp[++cnt] = st, tmp[0] = en_next;
-    for(int i = cnt; i >= 1; i--) g[tmp[i]].next = tmp[i-1];
-    block_merge(head);
-}
-void block_print(int k)
-{
-    int idx = head;
-    block_locate(idx, k);
-    if(k == g[idx].sz) idx = g[idx].next, k = 0;//输出字符时特判一下，用下面的输出函数不用特判
-    block_update(idx);
-    printf("%c\n", g[idx].s[k]);
-}
-void block_print(int k, int n, char *str)
-{
-    int idx = head;
-    block_locate(idx, k);
-    int len = 0;
-    for(int i = idx; i != -1 && n > 0; i = g[i].next)
-    {
-        block_update(i);
-        int sz = min(n, g[i].sz - k);
-        memcpy(str + len, g[i].s + k, sizeof(char) * sz);
-        len += sz, n -= sz;
-        k = 0;
+    char find(int pos){
+         int cur = head;
+         while ( pos - list[cur].size > 0){
+               pos -= list[cur].size;
+               cur = list[cur].next;
+         }
+         return list[cur].str[pos - 1];
     }
-    str[len] = '\0';
-    puts(str);
-}
-int main()
-{
-//    freopen("d://in.txt", "r", stdin);
-//    freopen("d://out1.txt", "w", stdout);
-    int T, i, j, n,cmd,pos;
-    char text[100000],ch[2];
-    ch[1] = '\0';
-    cin >> T;
-    for(i=0;i<T;i++){
-        block_init();
-        cin >> text;
-        cout << text << endl;
-        block_insert(0, strlen(text)+1, text);
-        cin >> n;
-        while(n--){
-            cin >> cmd;
-            if(cmd==1){
-                // insert
-                cin >> ch >> pos;
-                block_insert(pos, 1, ch);
-            }else{
-                // lookup
-                cin >> pos;
-                block_print(pos);
-            }
+}A;
+char str[MAXN];
+int n, T;
+ 
+int main() {
+    // freopen("test.in",  "r",  stdin);
+    // freopen("test.ans",  "w",  stdout); 
+    scanf("%d", &T);
+    while (T--){
+         
+    scanf("%s%d", str, &n);
+    A.init(str);
+    for (int i = 0; i < n; i++){
+        int pos, op;
+        scanf("%d", &op);
+        if (op == 1){
+           char S[2]; 
+           scanf("%s%d", S, &pos);
+           A.insert(pos - 1, S[0]);
+        } else {
+           scanf("%d", &pos);
+           printf("%c\n", A.find( pos ));
         }
-    }
-    int cur = 0;
-    char op[20];
-    
-    scanf("%d", &n);
-    while(n--)
-    {
-        scanf("%s", op);
-        if(op[0] == 'M') scanf("%d", &cur);
-        else if(op[0] == 'I')
-        {
-            int len;
-            scanf("%d", &len);
-            int i = 0;
-            while(i < len) {char ch = getchar();if(ch >= 32 && ch <= 126) str[i++] = ch;}
-            str[i++] = '\0';
-            
-        }
-        else if(op[0] == 'D')
-        {
-            int len;
-            scanf("%d", &len);
-            block_delete(cur, cur + len);
-        }
-        else if(op[0] == 'R')
-        {
-            int len;
-            scanf("%d", &len);
-            block_rev(cur, cur + len);
-        }
-        else if(op[0] == 'G')
-        {
-            
-            //block_print(cur, 1, str);
-        }
-        else if(op[0] == 'P') cur--;
-        else if(op[0] == 'N') cur++;
+    } 
     }
     return 0;
 }
--- lab03/G/solution.cpp
+++ lab03/G/solution.cpp
@@ -1,111 +1,62 @@
-#include <iostream>
-#include <list>
-// use only doublely linked list O(nlogn) for 1 quick sort and O(n) for iteration
-
+#include <bits/stdc++.h>
 using namespace std;
+
+int a[5010], b[5010], nxt[5010], bef[5010], h[100010];
 
 int main()
 {
-    int T, i, j, n;
-    cin >> T;
-    for (i = 0; i < T; i++)
-    {
-        cin >> n;
-        int int_tmp;
-        list<int> linkedlist;
-        list<int>::iterator node_seq[n], node_median, node_tmp;
-        // input the data
-        for (j = 0; j < n; j++)
-        {
-            cin >> int_tmp;
-            linkedlist.push_back(int_tmp);
-            node_tmp = linkedlist.end();
-            node_tmp--;
-            node_seq[j] = node_tmp;
-            //cout << "input " << node_tmp->data << endl;
-        }
-        // ensure the n is an odd number
-        if (n % 2 == 0)
-        {
-            n = n - 1;
-            linkedlist.pop_back();
-        }
-        // sort all the data
-        linkedlist.sort();
-        // get median node
-        node_median = linkedlist.begin();
-        advance(node_median, n / 2);
-        // tranvrse reversely
-        list<int>::iterator removed[2];
-        int move = 0, result[n], k = 0, odd = 1;
-        result[k++] = *node_median;
-        removed[0] = node_seq[n - 1];
-        for (j = n - 2; j > -1; j--)
-        {
-            //cout << "size" << linkedlist.size()<< endl;
-            if (odd)
-            {
-                removed[1] = node_seq[j];
-                odd = 0;
-            }
-            else
-            {
-                odd = 1;
-                // update median node
-                move = 0;
-                if (*removed[0] < *node_median && *removed[1] < *node_median)
-                {
-                    move = 1;
-                }
-                else if (*removed[0] > *node_median && *removed[1] > *node_median)
-                {
-                    move = -1;
-                }
-                else if (*removed[0] == *node_median)
-                {
-                    if (*removed[1] < *node_median)
-                    {
-                        move = 1;
-                    }
-                    else
-                    {
-                        move = -1;
-                    }
-                }
-                else if (*removed[1] == *node_median)
-                {
-                    if (*removed[0] < *node_median)
-                    {
-                        move = 1;
-                    }
-                    else
-                    {
-                        move = -1;
-                    }
-                }
-                //cout << "mid" << *node_median <<endl;
-                if (move != 0)
-                {
-                    do
-                    {
-                        //cout<<"move" << move <<endl;
-                        advance(node_median, move);
-                    } while (node_median == removed[0] || node_median == removed[1]);
-                }
-                //cout << "mid" << *node_median <<endl;
-                linkedlist.erase(removed[0]);
-                linkedlist.erase(removed[1]);
-                result[k++] = *node_median;
-                removed[0] = node_seq[j];
-            }
-            //cout << "rm" << "--"<<*node_seq[j] << endl;
-        }
-        //cout << "size" << linkedlist.size()<< endl;
-        for (j = k - 1; j > -1; j--)
-        {
-            cout << result[j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+	// freopen("test.in",  "r",  stdin);
+    // freopen("test.ans",  "w",  stdout); 
+	int T;
+	scanf("%d", &T);
+	while (T--)
+	{
+		memset(h, 0, sizeof(h));
+		int n;
+		scanf("%d", &n);
+		for (int i = 1; i <= n; ++i)
+		{
+			scanf("%d", &a[i]);
+			b[i] = a[i];
+			bef[i] = i - 1;
+			nxt[i] = i + 1;
+		}
+		nxt[0] = 1;
+		bef[n + 1] = n;
+		sort(b + 1, b + n + 1);
+		for (int i = 1; i <= n; ++i)
+			h[b[i]] = i;
+		
+		int p(n / 2 + 1);
+		vector<int > v;
+		v.push_back(b[p]);
+		
+		for (int i = n; i >= 1; --i)
+			if (i % 2 == 0)
+			{
+				int p1 = h[a[i]], p2 = h[a[i + 1]];
+				nxt[bef[p1]] = nxt[p1];
+				bef[nxt[p1]] = bef[p1];
+				nxt[bef[p2]] = nxt[p2];
+				bef[nxt[p2]] = bef[p2];
+				if (a[i] >= b[p] && a[i + 1] >= b[p])
+				{
+					p = bef[p];
+					v.push_back(b[p]);
+				}
+				else
+					if (a[i] <= b[p] && a[i + 1] <= b[p])
+					{
+						p = nxt[p];
+						v.push_back(b[p]);
+					}
+					else
+						v.push_back(b[p]);
+			}
+		for (int i = v.size() - 1; i >= 0; --i)
+			printf("%d ", v[i]);
+		putchar('\n');
+	}
+	
+	return 0;
 }
