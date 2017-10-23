bb5e41ec4763a6adf3ddb9be1356538ebf643439
edwardfang <fangyd1997@outlook.com>
2017-10-23 11:33:50 -0700

    finish D

--- lab05/D/solution.cpp
+++ lab05/D/solution.cpp
@@ -1,11 +1,40 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <string>
-#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
 using namespace std;
 
-int T, i, tmp;
-string str, pattern;
+int T, i, tmp, n, m;
+bool skip[1000010] = {false};
+string gen_new(string &str)
+{
+    int pos = 0, len = 0;
+    string newstr;
+    for (int j = 0; j < str.size(); j++)
+    {
+        //cout << "j" << j << "pos" << pos << "len" << len << endl;
+        if (!skip[j])
+        {
+            len++;
+        }
+        else
+        {
+            newstr.append(str.begin() + pos, str.begin() + pos + len);
+            //cout << newstr << endl;
+            len = 0;
+            pos = j + m;
+            skip[j] = false;
+            j += m - 1;
+        }
+    }
+    if (len > 0)
+    {
+        newstr.append(str.begin() + pos, str.begin() + pos + len);
+    }
+    return newstr;
+}
 
 vector<int> computePrefix(string pat)
 {
@@ -32,8 +61,8 @@ vector<int> computePrefix(string pat)
 int KMP(string &str, string pat)
 {
     int count = 0;
-    int n = str.size();
-    int m = pat.size();
+    n = str.size();
+    m = pat.size();
     vector<int> longestPrefix = computePrefix(pat);
     //int * longestPrefix = computePrefix(pat);
     for (int i = 0, k = 0; i < n; i++)
@@ -50,6 +79,8 @@ int KMP(string &str, string pat)
         if (k == m)
         {
             count++;
+            skip[i - m + 1] = true;
+            /*
             str.erase(i - m + 1, m);
             //cout << i - m + 2<< "gg"<< m << endl;
             if (i - 2 * m + 2 > -2)
@@ -60,25 +91,41 @@ int KMP(string &str, string pat)
             {
                 i = -1;
             }
-            k = 0;
-            
-            //k = longestPrefix[k - 1];
+            */
+            //k = 0;
+            k = longestPrefix[k - 1];
         }
     }
     return count;
 }
 
+char str_[1000010], pattern_[1000010];
+int finddu = 0;
 int main()
 {
-    ios::sync_with_stdio(false);
+    // std::ios::sync_with_stdio(false);
     cin.tie(0);
-    cin >> T;
+    //cin >> T;
+    scanf("%d", &T);
     for (i = 0; i < T; i++)
     {
-        cin >> tmp >> str;
-        cin >> tmp >> pattern;
-        KMP(str, pattern);
+        scanf("%d", &tmp);
+        scanf("%s", str_);
+        //cin >> tmp >> str;
+        scanf("%d", &tmp);
+        scanf("%s", pattern_);
+        //cin >> tmp >> pattern;
+        string str(str_), pattern(pattern_);
+        do
+        {
+            finddu = KMP(str, pattern);
+            str = gen_new(str);
+        } while (finddu != 0);
+
         //cout << KMP(str, pattern) << endl;
+        // strncpy(str_, str.c_str(), sizeof(str_));
+        // str_[sizeof(str_) - 1] = 0;
+        // printf("%s\n", str_);
         cout << str << endl;
     }
     return 0;
---
+++ lab05/E/README.md
@@ -0,0 +1,39 @@
+# Problem E: Hash the string!
+
+Time Limit: 1 Sec  Memory Limit: 128 MB
+
+## Description
+
+Do you know how to hash a string? Normally we represent a string in an integer. Then we can transfer from dealing with strings to dealing with integers, which is more convenient. A popular way of hashing a string is:Shash=∑|S0,i|*cnt(S0,i). where Shash is an integer, which is the hash value. S0,i is the substring of S from position 0 to position i. cnt(S0,i) is how many times S0,i appears in S. Now give you a string S, please calculate the hashed value Shash.
+
+## Input
+
+The first line will be an integer T, which is the number of test case. (1 <= T <= 10)
+
+The first line for each test case will be an integer n, which is the length of string S.
+
+Then there is a line with string S. |S| <= 1000000
+
+S will only contain lower case English letters.
+
+## Output
+
+Print an integer for each test case, the hashed value.
+
+## Sample Input
+
+```
+1
+3
+aaa
+```
+
+## Sample Output
+
+```
+10
+```
+
+## HINT
+
+Hard problem.
