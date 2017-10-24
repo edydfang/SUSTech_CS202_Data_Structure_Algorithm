d33ff4b6d07ade8a714e1a622f857ead9c7a1da9
edwardfang <fangyd1997@outlook.com>
2017-10-24 06:29:21 -0700

    finish E

--- lab05/E/solution.cpp
+++ lab05/E/solution.cpp
@@ -3,52 +3,60 @@
 
 using namespace std;
 
-vector<int> computePrefix(string pat){
+vector<int> computePrefix(string pat)
+{
     int m = pat.size();
     vector<int> longestPrefix(m);
-    for(int i = 1, k = 0; i < m; i++){
-        while(k > 0 && pat[k] != pat[i]){
+    longestPrefix[0] = 0;
+    for (int i = 1, k = 0; i < m; i++)
+    {
+        while (k > 0 && pat[k] != pat[i])
+        {
             k = longestPrefix[k - 1];
         }
-        if(pat[i] == pat[k]){
+        if (pat[i] == pat[k])
+        {
             longestPrefix[i] = ++k;
         }
-        else{
+        else
+        {
             longestPrefix[i] = k;
         }
         //cout << i << "--" << longestPrefix[i] << "||";
     }
+    // for (vector<int>::const_iterator i = longestPrefix.begin(); i != longestPrefix.end(); ++i)
+    // std::cout << *i << ' ';
     return longestPrefix;
 }
-int KMP(string str,string pat){
-    int count = 0;
-    int n = str.size();
+long solve(string pat)
+{
+    long hash = 0;
     int m = pat.size();
+    long count[m];
     vector<int> longestPrefix = computePrefix(pat);
-    for(int i = 0, k = 0; i < n; i++){
-        while(k > 0 && pat[k] != str[i]){
-            k = longestPrefix[k - 1];
-        }
-        if(str[i] == pat[k]){
-            k++;
-        }
-        if(k == m){
-            count ++ ;
-            //cout << i - m + 1 << "\n";
-            k = longestPrefix[k - 1];
-        }
+    for (int i = 0; i < m; i++)
+    {
+        count[i] = 1;
     }
-    return count;
+    for (int i = m - 1; i > -1; i--)
+    {
+        count[longestPrefix[i]-1] += count[i];
+        hash += (long)(i + 1) * count[i];
+        //cout << i+1 << "-" << count[i] << endl;
+    }
+    return hash;
 }
-int T,i,tmp;
-string str,pattern;
-int main(){
+int T, i, tmp;
+string str;
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> T;
-    for(i=0;i<T;i++){
-        cin >> tmp >> str >> tmp>>pattern;
-        cout << KMP(str, pattern) << endl;
+    for (i = 0; i < T; i++)
+    {
+        cin >> tmp >> str;
+        cout << solve(str) << endl;
     }
     return 0;
 }
